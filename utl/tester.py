8dcd77b6e5dcba2926c09009390997dbd68142be
edoss00 <edoss00@stuy.edu>
2019-10-25 05:49:31 -0400

    added blog functions

--- utl/tester.py
+++ utl/tester.py
@@ -11,6 +11,8 @@ from utl.db_builder import exec
 #for creating randomly generated id numbers
 limit = sys.maxsize
 
+#==========================user functions===========================
+
 #helper function
 def addUserHelper(username, password, data):
     rand = random.randrange(limit)
@@ -20,6 +22,7 @@ def addUserHelper(username, password, data):
     command = "INSERT INTO user_tbl VALUES(" + str(rand) + ",\"" + username + "\",\"" + password + "\")"
     #print(command)
     exec(command)
+
 
 #addUser
 def addUser(username, password):
@@ -32,6 +35,7 @@ def addUser(username, password):
         return True
     return False #if username already exists
 
+
 #helper function
 def verifyUserHelper(username, password, data):
     myEntry = (username, password)
@@ -40,38 +44,134 @@ def verifyUserHelper(username, password, data):
         return True
     return False
 
+
 #verifyUser
 def verifyUser(username, password):
     q = "SELECT username,password FROM user_tbl;"
     data = exec(q).fetchall()
     return verifyUserHelper(username,password,data)
 
+#==========================user get methods===========================
+
+#getUserID
 def getUserID(username):
     q = "SELECT user_id FROM user_tbl where username = '%s';" % username
     data = exec(q).fetchone()
     return data
 
+
+#getUserIDStr for str instead of tuple output
+def getUserIDStr(username):
+    user_id = getUserID(username)
+    return str(user_id[0])
+
+
+#getUserInfo
 def getUserInfo(user_id):
     q = "SELECT username FROM user_tbl where user_id = '%s';" % user_id
     data = exec(q).fetchone()
     return data
 
-###addUser tests
-##addUser("Elizabeth","wow")
-##addUser("Emily","this")
-##addUser("Jackie","actually")
-##addUser("Yaru","works")
-##
-###verifyUser tests
-##print(verifyUser("Elizabeth","wow"))    #True
-##print(verifyUser("Emily","this"))       #True
-##print(verifyUser("Elizabeth","wow"))    #True
-##print(verifyUser("Elizabeth","wo"))     #False
-##print(verifyUser("Elizbeth","wow"))     #False
-##
-###getUser tests
-##print(getUser("Elizabeth"))
-##print(getUser("Emily"))
-##print(getUser("Yaru"))
-##print(getUser("Jackie"))
-##print(getUser("wow"))
+
+#getUserInfo
+def getUserPass(user_id):
+    q = "SELECT password FROM user_tbl where user_id = '%s';" % user_id
+    data = exec(q).fetchone()
+    return data
+
+#==========================user tests===========================
+
+#addUser - doesn't matter if commented out because checks if user already exists
+addUser("Elizabeth","wow")
+addUser("Emily","this")
+addUser("Jackie","actually")
+addUser("Yaru","works")
+
+
+#verifyUser
+print(verifyUser("Elizabeth","wow"))    #True
+print(verifyUser("Elizabeth","wo"))     #False
+
+
+#getUser
+print(getUserID("Elizabeth"))
+print(getUserIDStr("Emily"))
+print(getUserID("wow"))        #None
+
+#==========================blog functions===========================
+
+#joins user_id for user_tbl and blog_tbl
+command="SELECT user_tbl.user_id FROM user_tbl LEFT JOIN blog_tbl ON user_tbl.user_id = blog_tbl.user_id"
+exec(command)
+
+
+#helper function
+def addNewBlogHelper(user_id, title, time_created, data):
+    rand = random.randrange(limit)
+    while (rand in data):
+        rand = random.randrange(limit)
+    #print(rand)
+    command = "INSERT INTO blog_tbl VALUES(" + str(rand) + "," + user_id + ",\"" + title + "\",\"" + time_created + "\",\"" + time_created + "\")"
+    #print(command)
+    exec(command)
+
+
+#addNewBlog
+def addNewBlog(username, title, time_created):
+    user_id = getUserIDStr(username) 
+    q = "SELECT title FROM blog_tbl where user_id = '%s';" % user_id
+    data = exec(q).fetchall()
+    addNewBlogHelper(user_id, title, time_created, data)
+
+
+def updateBlog(username, title, time_edited):
+    user_id = getUserIDStr(username) 
+    command = "UPDATE blog_tbl SET time_updated=\"" + time_edited + "\" WHERE title=\"" + title + "\" AND user_id=" + user_id + ";"
+    print(command)
+    exec(command)
+
+#==========================blog get methods===========================
+    
+#getBlogID
+def getBlogID(username, title):
+    user_id = getUserIDStr(username)
+    q = "SELECT blog_id FROM blog_tbl WHERE user_id =" + user_id + " AND title=" + title + ";"
+    data = exec(q).fetchone()
+    return data
+
+
+#getBlogIDStr for str instead of tuple output
+def getBlogIDStr(username, title):
+    blog_id = getBlogID(username, title)
+    return str(user_id[0])
+
+
+#getBlogTitle
+def getBlogTitle(blog_id):
+    q = "SELECT title FROM blog_tbl WHERE blog_id =" + blog_id + ";"
+    data = exec(q).fetchone()
+    return data
+
+#==========================blog get methods===========================
+
+##addNewBlog
+#addNewBlog("Elizabeth", "Doss", "2019-10-25 04:22")
+#addNewBlog("Jackie", "Lin", "2019-10-25 04:58")
+#addNewBlog("Yaru", "Luo", "2019-10-25 04:59")
+#addNewBlog("Yaru", "Lao", "2019-10-25 05:27")
+
+
+##updateBlog
+#updateBlog("Elizabeth", "Doss", "2019-10-25 05:09")
+#updateBlog("Yaru", "Luo", "2019-10-25 05:20")
+
+
+##DOESN'T WORK
+##sqlite3.OperationalError: no such column: Doss
+#print(getBlogID("Elizabeth", "Doss"))
+#print(getBlogID("Yaru", "Lao"))
+
+
+
+
+
