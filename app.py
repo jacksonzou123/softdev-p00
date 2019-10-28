480415aa803a72324b34b19f413377b12d54f680
Jackie Lin <jlin00@stuy.edu>
2019-10-28 08:32:31 -0400

    added comments

--- app.py
+++ app.py
@@ -18,14 +18,17 @@ def root():
         return redirect(url_for('profile'))
     return redirect(url_for('sign')) #else go to sign-in options
 
+#welcome page with sign-in options
 @app.route("/welcome")
 def sign():
     return render_template('welcome.html')
 
+#register for an account
 @app.route("/register")
 def register():
     return render_template('register.html')
 
+#process register form
 @app.route("/adduser", methods=['POST'])
 def adduser():
     username = request.form['username']
@@ -37,25 +40,27 @@ def adduser():
     #print(request.method)
     #print(username)
     #print(password)
-    if (password != confirm):
+    if (password != confirm): #passwords do not match
         flash('Passwords do not match!', 'red')
         return redirect(url_for('register'))
-    added = tester.addUser(username, password)
+    added = tester.addUser(username, password) #returns True if added successfully, false if username take
     if (added):
         flash('You have been registered successfully. Please log in.', 'green')
         return redirect(url_for('login'))
     flash('Username already taken.', 'red')
     return redirect(url_for('register'))
 
+#log in to account
 @app.route("/login")
 def login():
     return render_template('login.html')
 
+#process login form
 @app.route("/auth", methods=['POST'])
 def auth():
     username = request.form['username']
     password = request.form['password']
-    verified = tester.verifyUser(username, password)
+    verified = tester.verifyUser(username, password) #returns True if logged in successfully, false if otherwise
     if (verified):
         id = tester.getUserIDStr(username)
         session["userid"] = id
@@ -66,10 +71,10 @@ def auth():
 
 ## USER MAY NOT PROCEED PAST HERE WITHOUT BEING LOGGED IN ##
 
-#checks if user is logged in
-
+#user profile page
 @app.route("/user")
 def profile():
+    #this code is reused throughout all the functions to check if user is logged in
     if ("userid" not in session):
       flash('You must log in to access this page!', 'red')
       return redirect(url_for('login'))
@@ -77,19 +82,20 @@ def profile():
     if (request.args): #if url has query string
         if ('id' in request.args): #if url has id
             id = request.args['id']
-            name = tester.getUserInfo(id)
+            name = tester.getUserInfo(id) #get username from id query
             if (name): #if id is valid user id
-                username = name[0]
-                isOwner = False
-                if (id == session['userid']):
+                username = name[0] #retrieve username from tuple
+                isOwner = False #checks if logged in user "owns" the profile
+                if (id == session['userid']): #if logged in user matches owner of blog
                     isOwner = True
-                blogs = tester.getUserBlogs(id)
-                return render_template('profile.html', username=username, owner=username, isOwner=isOwner, blogs = blogs) #success!
-            flash('No profile was found for the given url. You have been redirected back to your own profile.', 'red')
+                blogs = tester.getUserBlogs(id) #get all blogs based on id query
+                return render_template('profile.html', username=username, owner=username, isOwner=isOwner, blogs=blogs) #success!
+            flash('No profile was found for the given url. You have been redirected back to your own profile.', 'red') #if id is not valid user_id
         else:
-            flash('No profile was found for the given url. You have been redirected back to your own profile.', 'red')
-    return redirect(url_for('profile', id=session["userid"]))
+            flash('No profile was found for the given url. You have been redirected back to your own profile.', 'red') #if url does not have id query
+    return redirect(url_for('profile', id=session["userid"])) #if url does not have querystring, return to "My Profile"
 
+#about page with instructions on running the site
 @app.route("/about")
 def about():
     if ("userid" not in session):
@@ -98,29 +104,31 @@ def about():
     username = tester.getUserInfo(session['userid'])[0]
     return render_template('about.html', username=username)
 
+#search page with search bar
 @app.route("/search")
 def search():
     if ("userid" not in session):
       flash('You must log in to access this page!', 'red')
       return redirect(url_for('login'))
-    #QUERY STRING
     username = tester.getUserInfo(session['userid'])[0]
-    results = []
-    if (request.args):
+    results = [] #when user first visits search page, no results are displayed
+    if (request.args): #if user has made a search
         if ('query' in request.args):
-            query = request.args['query']
-            results = tester.findBlog(query)
+            query = request.args['query'] #search keyword
+            results = tester.findBlog(query) #results of search
             #print(results)
     return render_template('search.html', username=username, results=results)
 
+#process search query
 @app.route("/query", methods=['POST'])
 def query():
     if ("userid" not in session):
       flash('You must log in to access this page!', 'red')
       return redirect(url_for('login'))
     query = request.form['keyword']
-    return redirect(url_for('search', query=query))
+    return redirect(url_for('search', query=query)) #display results on search page
 
+#form to create a blog
 @app.route("/createblog")
 def createblog():
     if ("userid" not in session):
@@ -129,67 +137,70 @@ def createblog():
     username = tester.getUserInfo(session['userid'])[0]
     return render_template('createblog.html', username=username)
 
+#process form to create a blog
 @app.route("/updateblog", methods=['POST'])
 def updateblog():
     if ("userid" not in session):
       flash('You must log in to access this page!', 'red')
       return redirect(url_for('login'))
     title = request.form['title']
-    if (title == ''):
+    if (title == ''): #if no title entered
         flash('Fields cannot be empty', 'red')
         return redirect(url_for('createblog'))
     #print(title)
-    id = tester.addBlog(session['userid'], title)
+    id = tester.addBlog(session['userid'], title) #returns None if blog title used already by that user, else ID of blog created
     if (id == None):
         flash('You have already used this title', 'red')
         return redirect(url_for('createblog'))
     return redirect(url_for('blog', id=id))
 
+#blog page which also displays all entries in the blog
 @app.route("/blog")
 def blog():
     if ("userid" not in session):
       flash('You must log in to access this page!', 'red')
       return redirect(url_for('login'))
-    #QUERY STRING
-    if (request.args):
-        if ('id' in request.args):
+    if (request.args): #if querystring exists
+        if ('id' in request.args): #if id query exists
             id = request.args['id']
-            name = tester.getBlogTitleStr(id)
-            if (name):
-                session['blogid'] = id
-                isOwner = False
-                user_id = tester.getUserfromBlog(id)[0]
-                username = tester.getUserInfo(user_id)[0]
-                if (int(user_id) == int(session['userid'])):
+            name = tester.getBlogTitleStr(id) #get blog title from ID, returns None if ID is not associated with any blog
+            if (name): #if blog_id is associated with valid blog
+                session['blogid'] = id #store current blog that is being visited in session, useful for knowing where to add entries later without using queries
+                isOwner = False #default value
+                user_id = tester.getUserfromBlog(id)[0] #get user_id associated with blog_id
+                username = tester.getUserInfo(user_id)[0] #get username from user_id
+                if (int(user_id) == int(session['userid'])): #if owner of blog matches logged in user
                     isOwner = True
-                userlink = "/user?id=%s" % user_id
-                entries = tester.getAllEntries(id)
+                userlink = "/user?id=%s" % user_id #click link to access user profile from blog
+                entries = tester.getAllEntries(id) #get all entries from blog based on blog_id
                 return render_template('blog.html', username=username, isOwner=isOwner, title=name, userlink=userlink, entries=entries)
-            else:
+            else: #blog_id given in query is not valid
                 flash('No blog was found for the given URL. You have been redirected back to your own profile.', 'red')
-        else:
+        else: #no id query found
             flash('No blog was found for the given URL. You have been redirected back to your own profile.', 'red')
-    else:
+    else: #no querystring
         flash('No blog was found for the given URL. You have been redirected back to your own profile.', 'red')
     return redirect(url_for('profile', id=session['userid']))
 
+#form to create a blog entry
 @app.route("/editentry")
 def editentry():
     if ("userid" not in session):
       flash('You must log in to access this page!', 'red')
       return redirect(url_for('login'))
     username = tester.getUserInfo(session['userid'])[0]
-    temp_title = ""
-    temp_content = ""
-    link = "/updateentry"
-    if (request.args):
-        if ('id' in request.args):
-            id = request.args['id']
-            temp_title = tester.getEntryTitle(id)
-            temp_content = tester.getEntryContent(id)
-            link = link + "?id=%s" % id
+    temp_title = "" #if starting a new entry, blank title
+    temp_content = "" #if starting a new entry, blank content
+    link = "/updateentry" #link to process form
+    if (request.args): #if query string exists (editing an entry)
+        if ('id' in request.args): #if id query exists
+            id = request.args['id'] #if editing an entry, URl will contain ID querystring
+            temp_title = tester.getEntryTitle(id) #pre-fill title box with current title
+            temp_content = tester.getEntryContent(id) #pre-fill content box with current content
+            link = link + "?id=%s" % id #generate link to go to process form
     return render_template('createentry.html', username=username, temp_title=temp_title, temp_content=temp_content, link=link)
 
+#process form to create a blog entry
 @app.route("/updateentry", methods=['POST'])
 def updateentry():
     if ("userid" not in session):
@@ -198,26 +209,27 @@ def updateentry():
     title = request.form['title']
     content = request.form['content']
     id = session['blogid']
-    if (title == '' or content == ''):
+    if (title == '' or content == ''): #if empty submission
         flash('Fields cannot be empty', 'red')
         return redirect(url_for('editentry'))
-    if (request.args):
-        if ('id' in request.args):
+    if (request.args): #if query string exists (editing an entry)
+        if ('id' in request.args): #if id query exists
             entry_id = request.args['id']
-            entry = tester.editEntry(entry_id, title, content)
+            entry = tester.editEntry(entry_id, title, content) #edit entry, returns False if name is already being used
             if (not entry):
                 flash("Title is already being used for another post", 'red')
                 return redirect(url_for('editentry', id=entry_id))
-    else:
-        entry = tester.addEntry(id, title, content)
+    else: #if creating a new entry
+        entry = tester.addEntry(id, title, content) #returns None if title is already used for that blog
         if (entry == None):
             flash("You have already used this heading", 'red')
             return redirect(url_for('editentry'))
     return redirect(url_for('blog', id=id))
 
+#logout page
 @app.route("/logout")
 def logout():
-    session.clear()
+    session.clear() #clear info stored in session 
     flash('You have been successfully logged out.', 'green')
     return redirect(url_for('root'))
 
